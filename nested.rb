a00aa5f344e7e1cd69b8a0482b7854ce38ace25e
limerentfox <christi@limerentfox.com>
2016-11-15 17:24:03 +0900

    simple-nesting lab complete

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+ 
 def hopper
 	programmer_hash = 
  		{
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,7 +85,12 @@ def adding_matz
         }
      }
 
+     programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+     }
     
+    programmer_hash
 end
 
 def changing_alan
@@ -101,6 +111,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+
+     programmer_hash
      
      
 end
@@ -122,6 +136,8 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
      
 end
 
